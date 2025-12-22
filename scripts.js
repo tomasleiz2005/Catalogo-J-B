169576ec7df0eaf9e57b278916c6c97c33961059
tomasleiz2005 <betyeli61@gmail.com>
2025-12-22 12:04:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,1 @@
-// Espera a que la página cargue
-window.onload = function() {
-    // Es mejor usar rutas relativas para que funcione en cualquier PC
-    let backgroundUrl = 'Patagonia.jpg'; 
-    
-    let img = new Image();
-    img.src = backgroundUrl;
-    
-    img.onload = function() {
-        document.body.style.backgroundImage = 'url(' + backgroundUrl + ')';
-        console.log("Fondo cargado correctamente");
-    };
-    
-    img.onerror = function() {
-        console.error("No se pudo encontrar la imagen de fondo. Verifica que 'Patagonia.jpg' esté en la misma carpeta.");
-    };
-};
+
